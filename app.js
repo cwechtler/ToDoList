b5884b56057c5cc6c42994fa876e3f401e8dfe0c
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2020-08-27 14:10:14 -0500

    Add EJS

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    let currentDay = today.toLocaleDateString("en-US", options);
+    
+    res.render("list", {
+        dayOfWeek: currentDay
+    });
+
 });
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log(`Server has started on port: ${port}`);
 });
