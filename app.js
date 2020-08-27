af24ff6fccc9dba66e6b62053426935f000f34e0
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2020-08-27 14:25:35 -0500

    Add Dynamic ToDo List

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
@@ -19,9 +22,17 @@ app.get("/", function (req, res) {
     let currentDay = today.toLocaleDateString("en-US", options);
     
     res.render("list", {
-        dayOfWeek: currentDay
+        dayOfWeek: currentDay,
+        newListItems: items
     });
 
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newListItem;
+    items.push(item);
+
+    res.redirect("/");
 });
 
 app.listen(port, function () {
